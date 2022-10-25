624e92d078bee0892007f58b58faac6d12232649
Ben Njoroge <ben.njoroge@bettyblocks.com>
2022-10-25 11:41:54 +0200

    Add imanage-upload-document function

---
+++ actions/imanage/functions/imanage-upload-document/1.0/index.js
@@ -0,0 +1,7 @@
+import FormData from 'form-data'
+
+const imanageUploadDocument = async ({documentName, documentId, authToken, file}) => {
+    console.log({documentName, documentId, authToken, file})
+}
+
+export default imanageUploadDocument;
