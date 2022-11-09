af0b3f34d2585e603b6868242eeed3f2ee818037
Johannes Wielinga <johannes.wielinga@bettyblocks.com>
2022-11-09 16:05:04 +0100

    chore: update modules

--- highq/datasource-actions.edge/functions/highq/1.0/models.js
+++ highq/datasource-actions.edge/functions/highq/1.0/models.js
@@ -3,14 +3,64 @@ const I_SHEET = {
     path: "/{version}/isheet/template",
     method: "GET",
   },
+  getAllColumns: {
+    path: "/{version}/isheets/{isheetid}/columns",
+    method: "GET",
+  },
   getAllIsheets: {
     path: "/{version}/isheets",
     method: "GET",
   },
-  // isheetRecordID
+  getOneTemplateISheet: {
+    path: "/{version}/isheet/templateisheet?templateid={templateid}",
+    method: "GET",
+  },
   getAllExternalId: {
     path: "/{version}/files/{fileId}/isheetRecordID",
     methos: "GET",
+  },
+};
+
+const DOCUMENT_REOSURCES = {
+  getOneFileDetail: {
+    path: "/{version}/files/{fileid}",
+    method: "GET",
+  },
+  getOneFolder: {
+    path: "/{version}/folders/{folderid}",
+    method: "GET",
+  },
+  getOneFile: {
+    path: "/{version}/files/{fileid}/content",
+    method: "GET",
+  },
+  getAllFile: {
+    path: "/{version}/files",
+    method: "GET",
+  },
+  getAllFolders: {
+    path: "/{version}/folders/{folderid}/files",
+    method: "GET",
+  },
+  getAllFolderPermissions: {
+    path: "/{version}/folders/{folderid}/permissions",
+    method: "GET",
+  },
+  getAllAudits: {
+    path: "/{version}/files/{fileid}/audit",
+    method: "GET",
+  },
+  getAllFolderPermissions: {
+    path: "/{version}/files/{fileid}/permissions",
+    method: "GET",
+  },
+  getAllStatusLists: {
+    path: "/{version}/files/statuslist",
+    method: "GET",
+  },
+  getAllChanges: {
+    path: "/{version}/changes",
+    method: "GET",
   },
 };
 
@@ -28,4 +78,5 @@ export const MODEL_LOOKUP = {
     method: "GET",
   },
   ...I_SHEET,
+  ...DOCUMENT_REOSURCES,
 };
